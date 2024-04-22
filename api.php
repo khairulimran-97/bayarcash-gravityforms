90adc80c6fda63874201b99af565a9c6a1b95a2f
Khairul Imran <105085586+khairulimran-97@users.noreply.github.com>
2024-04-22 16:58:21 +0800

    Add cron requery

--- api.php
+++ api.php
@@ -11,7 +11,7 @@ class GFBayarcashAPI
 {
     private static $_instance;
 
-    public static function get_instance($fpx_portal_key, $pat_key)
+    public static function get_instance($fpx_portal_key, $pat_key): GFBayarcashAPI
     {
         if (self::$_instance == null) {
             self::$_instance = new self($fpx_portal_key, $pat_key);
@@ -26,7 +26,7 @@ class GFBayarcashAPI
         $this->pat = $pat_key;
     }
 
-    public function create_payment($postData)
+    public function create_payment($postData): void
     {
     $redirectUrl = GF_BAYARCASH_ROOT_URL . '/transactions/add';
 
@@ -44,8 +44,62 @@ class GFBayarcashAPI
     exit;
     }
 
+    public function get_payment($pat_key, $target_return_url)
+    {
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://console.bayarcash.dev/api/transactions',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_HTTPHEADER => array(
+                'Accept: application/json',
+                "Authorization: Bearer $pat_key"
+            ),
+        ));
 
+        $response = curl_exec($curl);
 
-    
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+
+        $target_record_id = null;
+        $exchange_order_no = null;
+        $fpx_amount = null;
+        $fpx_status = null;
+        $transaction_status_description = null;
+
+        foreach ($response['output']['transactionsList']['recordsListData'] as $record) {
+            if ($record['return_url'] === $target_return_url) {
+                $target_record_id = $record['id'];
+                break;
+            }
+        }
+
+        if ($target_record_id !== null) {
+            foreach ( $response['output']['transactionsList']['recordsListData'] as $record ) {
+                if ( $record['id'] === $target_record_id ) {
+                    $exchange_order_no = $record['exchange_order_no'];
+                    $fpx_amount = $record['amount'];
+                    $fpx_status = $record['status'];
+                    $transaction_status_description = $record['status_description'];
+                    break;
+                }
+            }}
+
+        // Returning relevant data
+        return array(
+            'exchange_order_no' => $exchange_order_no,
+            'fpx_amount' => $fpx_amount,
+            'fpx_status' => $fpx_status,
+            'transaction_status_description' => $transaction_status_description
+        );
+    }
+
 }
